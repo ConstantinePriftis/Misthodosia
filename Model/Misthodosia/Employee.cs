b85f1cbfa1d170143e814f6866a2d1850f8fb0cb
Constantine <priftiskonstantinos@ymail.com>
2016-03-11 11:19:37 +0200

    Commit after boom

---
+++ Model/Misthodosia/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misthodosia
+{
+   public  class Employee
+    {
+        public int ID { get; set; }        
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public decimal Wage { get; set; }
+        public Employee(int id ,string name , string surName , decimal wage)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.SurName = surName;
+            this.Wage = wage;
+        }
+    }
+}
---
+++ Model/Misthodosia/Reader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Misthodosia
+{
+    public class Reader
+    {
+        Employee[] employees;
+        public Reader()
+        {
+            employees = new Employee[5];
+        }
+        
+        public void PopulateEmployees()
+        {
+            int counter = 0;
+            foreach (string line in File.ReadLines("EmployeesDatabase.txt"))
+            {
+                string[] tmp = line.Split(',');
+                int tmpid = Convert.ToInt32(tmp[0]);
+                string tmpname = tmp[1];
+                string tmpSurname = tmp[2];
+                decimal tmpWage = Convert.ToDecimal(tmp[3]);
+                employees[counter] = new Employee(tmpid, tmpname, tmpSurname, tmpWage);
+                counter++;
+            }
+        }
+        public Employee[] GetAllEmployees()
+        {
+            return employees;
+            
+        }
+        
+    }
+}
